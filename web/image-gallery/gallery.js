6654fe0d948709628e852b9e2b13f2c35eead1f8
hoangdesu <hoangdesu@gmail.com>
2025-09-29 17:10:01 +0700

    image-gallery project. javascript. refactor changeImage function, optimize image loading code, dynamic rendering

--- web/image-gallery/gallery.js
+++ web/image-gallery/gallery.js
@@ -2,47 +2,78 @@ const backBtn = document.querySelector('#back-btn');
 const nextBtn = document.querySelector('#next-btn');
 const imageView = document.querySelector('#image-view');
 
-const images = [
+const imageLinks = [
   'https://cdn.motor1.com/images/mgl/qkk7zv/s3/novitec-ferrari-sf90-stradale-2022.jpg',
   'https://cdn.ferrari.com/cms/network/media/img/resize/68c0011f30b362002146768c?width=960&height=540',
   'https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/face_react/ad_personam/2024/gallery/ad_personam_01_m.jpg',
   'https://images.pistonheads.com/nimg/47438/blobid0.jpg',
   'https://www.pcarmarket.com/static/media/uploads/galleries/photos/uploads/galleries/20981-novak-2021-mercedes-benz-amg-gt-black-series/.thumbnails/_IMG7102.webp/_IMG7102-1200x0.webp',
+  'https://www.topgear.com/sites/default/files/2023/03/pagani1.jpg',
+  'https://xehay.vn/uploads/images/2024/1/01/xehay_Koenigsegg%20Jesko%20Odin_040123_6.jpg'
 ];
+
+// List of image objects 
+const images = imageLinks.map(link => {
+  const img = document.createElement('img');
+  img.src = link;
+  return img;
+});
+
+console.log(images);
+
+// Check if image has successfully loaded. Can be used to display a loading spinner
+for (const [i, img] of images.entries()) {
+  img.addEventListener('load', () => {
+    console.log('loaded ' + i);
+  });
+}
+
+// Dynamic rendering
+const thumbnailsContainer = document.querySelector('#thumbnails-container');
+
+for (const imageElement of images) {
+  thumbnailsContainer.append(imageElement);
+}
+
 
 let currentIndex = 0;
 
-nextBtn.addEventListener('click', () => {
-  if (currentIndex < images.length - 1) {
-    currentIndex++;
-    imageView.src = images[currentIndex];
+const changeImage = (newIndex) => {
+  if (currentIndex >= 0 && currentIndex <= images.length) {
+    currentIndex += newIndex;
+    // imageView.src = images[currentIndex];
+
+    // clear all elements inside the div
+    imageView.innerHTML = '';
+
+    // add the new img to the container (clone to avoid moving from thumbnails)
+    // When you call append() on a DOM element that's already
+    // in the document, it moves the element from its current
+    // location to the new location.
+    imageView.append(images[currentIndex].cloneNode());
+
+
+      // check buttons
+    if (currentIndex === 0) {
+      backBtn.disabled = true;
+    } else if (currentIndex === images.length - 1) {
+      nextBtn.disabled = true;
+    } else {
+      backBtn.disabled = false;
+      nextBtn.disabled = false;
+    }
   }
 
-  //   disabled buttons
-  if (currentIndex == 0) {
-    backBtn.disabled = true;
-  } else if (currentIndex == images.length - 1) {
-    nextBtn.disabled = true;
-  } else {
-    backBtn.disabled = false;
-    nextBtn.disabled = false;
-  }
+  console.log('current index', currentIndex);
+  console.log('thumbnails:', images);
+}
+
+nextBtn.addEventListener('click', () => {
+  changeImage(1);
 });
 
 backBtn.addEventListener('click', () => {
-  if (currentIndex > 0) {
-    currentIndex--;
-    imageView.src = images[currentIndex];
-  }
-
-  if (currentIndex == 0) {
-    backBtn.disabled = true;
-  } else if (currentIndex == images.length - 1) {
-    nextBtn.disabled = true;
-  } else {
-    backBtn.disabled = false;
-    nextBtn.disabled = false;
-  }
+  changeImage(-1);
 });
 
 document.addEventListener('keydown', (evt) => {
@@ -50,22 +81,11 @@ document.addEventListener('keydown', (evt) => {
 
   if (evt.key === 'ArrowLeft') {
     if (currentIndex > 0) {
-      currentIndex--;
-      imageView.src = images[currentIndex];
+      changeImage(-1);
     }
   } else if (evt.key === 'ArrowRight') {
     if (currentIndex < images.length - 1) {
-      currentIndex++;
-      imageView.src = images[currentIndex];
+      changeImage(1);
     }
-  }
-
-  if (currentIndex == 0) {
-    backBtn.disabled = true;
-  } else if (currentIndex == images.length - 1) {
-    nextBtn.disabled = true;
-  } else {
-    backBtn.disabled = false;
-    nextBtn.disabled = false;
   }
 });
