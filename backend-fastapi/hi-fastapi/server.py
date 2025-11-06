644892fd57df3fd9ed3451e086d4607c3bc57066
hoangdesu <hoangdesu@gmail.com>
2025-11-06 21:00:38 +0700

    shell script, environment, return schema, implemented PUT and DELETE handlers

--- backend-fastapi/hi-fastapi/server.py
+++ backend-fastapi/hi-fastapi/server.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, List
 
 from fastapi import FastAPI, status
 from fastapi.responses import HTMLResponse, JSONResponse
@@ -17,15 +17,12 @@ def read_root():
     return {"Hello": "World"}
 
 
-
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     if item_id < len(games):
         return {"game": games[item_id], "q": q}
     else:
         return {"message": "error"}
-    
     
 
 @app.get('/hi', response_class=HTMLResponse)
@@ -37,19 +34,18 @@ def hi_page():
 # /add/a/b
 @app.get('/add/{a}/{b}')
 def add(a: int, b: int):
-    
     return {"total": a + b}
 
 
 
 @app.get('/games')
-def get_games():
+def get_games() -> List[str]:
     return games
 
 
-@app.get('/games/{id}')
-def get_game(id: int):
-    return games[id] if id < len(games) else "id error"
+@app.get('/games/{index}')
+def get_game(index: int):
+    return games[index] if id < len(games) else "index error"
 
 
 
@@ -61,12 +57,32 @@ class Game(BaseModel):
 @app.post('/games', status_code=status.HTTP_201_CREATED)
 def add_game(new_game: Game):
 
-    if new_game.name not in games:
-        games.append(new_game.name)
-        return games
-
-    
-    return JSONResponse(
+    # Guarding
+    if new_game.name in games:
+        return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
             content={"message": "Duplicated game"}
         )
+        
+    games.append(new_game.name)
+    return games
+
+
+
+# games.pop()
+
+
+# PUT
+# PUT /games
+# body -> {
+#     current index to be changed: int,
+#     new data to update: str
+# }
+
+# PUT /games/{index}
+# body -> {
+#     new data to update: str
+# }
+
+
+# DELETE 
